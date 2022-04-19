8a0fb75d3e60d04be137063f4daf68734b037c49
Daniel's kiala <danielskiala@gmail.com>
2022-04-19 12:45:38 +0200

    First-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/site/about/about.component';
+import { ContactComponent } from './components/site/contact/contact.component';
+import { DonationComponent } from './components/site/donation/donation.component';
+import { GaleryComponent } from './components/site/galery/galery.component';
+import { HomeComponent } from './components/site/home/home.component';
+;
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'site/home', pathMatch: 'full' },
+  {path:"site/home", component:HomeComponent},
+  {path:"site/galery", component:GaleryComponent},
+  {path:"site/about", component:AboutComponent},
+  {path:"site/contact", component:ContactComponent},
+  {path:"site/donation", component:DonationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/site/home/home.component';
+import { NavComponent } from './components/site/nav/nav.component';
+import { FooterComponent } from './components/site/footer/footer.component';
+import { GaleryComponent } from './components/site/galery/galery.component';
+import { AboutComponent } from './components/site/about/about.component';
+import { ContactComponent } from './components/site/contact/contact.component';
+import { DonationComponent } from './components/site/donation/donation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent,
+    GaleryComponent,
+    AboutComponent,
+    ContactComponent,
+    DonationComponent
   ],
   imports: [
     BrowserModule,
